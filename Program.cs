30f012d0b3fc34419cb3934cb67a8c4367c2080d
inkoz <inkoz@DESKTOP-JS5JFS1>
2022-07-15 18:31:36 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void FillArrayRandomNumbers(int[,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = new Random().Next(0, 10);
+                }
+            }
+        }
+
+        static void PrintArray(int[,] array)
+        {
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                Console.Write("[ ");
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.Write("]");
+                Console.WriteLine("");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите количество строк");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("введите количество столбцов");
+            int m = Convert.ToInt32(Console.ReadLine());
+
+            int[,] numbers = new int[n, m];
+            FillArrayRandomNumbers(numbers);
+
+
+            for (int j = 0; j < numbers.GetLength(1); j++)
+            {
+                double avarage = 0;
+                for (int i = 0; i < numbers.GetLength(0); i++)
+                {
+                    avarage = (avarage + numbers[i, j]);
+                }
+                avarage = avarage / n;
+                Console.Write(avarage + "; ");
+            }
+            Console.WriteLine();
+            PrintArray(numbers);
+
+
+
+        }
+    }
+}
